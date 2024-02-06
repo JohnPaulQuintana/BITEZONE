a54c16f2da28ddf05e4b4ff6a96261bcb99d0e98
abirmehedi77 <86663285+abirmehedi77@users.noreply.github.com>
2024-02-06 21:31:53 +0800

    Initial commit

---
+++ resources/views/includes/main.blade.php
@@ -0,0 +1,7 @@
+
+<div class="border grid grid-cols-2 h-auto p-3 bg-img">
+    <div class="border flex justify-end">
+        <img class="border" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGfy70ajGs7IZnGMilz5TWAf8cl_e0FyyyzttKK9QWbAzcTQHgBKNtpCm1c2bu0M_I0xQ&usqp=CAU" alt="">
+    </div>
+    <div class="border text-start"><i class="fa-solid fa-check"></i> Contents lore</div>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>BITEZONE</title>
+
+        {{-- font-awesome --}}
+        <link rel="stylesheet" href="{{ asset('font-awesome/all.min.css') }}">
+
+        @vite('resources/css/app.css')
+        
+        {{-- xustom css --}}
+        <style>
+            .bg-img{
+                background: url("{{ asset('images/test.jpg') }}");
+                background-repeat:no-repeat;
+                background-size: contain;
+                background-position: right;
+            }
+        </style>
+    </head>
+    <body>
+        {{-- header --}}
+        <header class="border-black border-b-2 flex gap-2 justify-between p-2">
+            <div class="logo border">Logo</div>
+            <div class="nav border">
+                <ul class="flex gap-2">
+                    <li class="cursor-pointer hover:text-green-600">navigation</li>
+                    <li>navigation</li>
+                    <li>navigation</li>
+                    <li>navigation</li>
+                </ul>
+            </div>
+            
+        </header>
+        <div class="">
+            {{-- @if (Route::has('login'))
+                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif --}}
+
+            {{-- main --}}
+            <div>
+                @include('includes.main')
+            </div>
+
+
+
+            {{-- footer --}}
+            <div class="footer">
+
+            </div>
+            
+        </div>
+
+        {{-- scripts --}}
+        <script src="{{ asset('font-awesome/all.min.js') }}"></script>
+    </body>
+</html>

42642db6f936e2470ec0ee7d3b78769ecfe28de3
abirmehedi77 <86663285+abirmehedi77@users.noreply.github.com>
2024-02-09 00:36:06 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,56 +55,10 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         
-        {{-- xustom css --}}
-        {{-- <style>
-            body{
-                padding: 0;
-                margin: 0;
-                box-sizing: border-box
-            }
-            #logo{
-                height: 45px;
-            }
-            
-        </style> --}}
+    
     </head>
     <body>
-        {{-- header --}}
-        {{-- <header class="bg-red-500 border-red-300 text-white border-b-2 flex gap-2 justify-between p-2 fixed w-full z-50">
-            <div class="flex gap-2">
-                <i class="fa-solid fa-paw fa-2xl"></i>
-                <h1 class="font-bold text-4xl"> BITEZONE</h1>
-            </div>
-            <div>
-                @if (Route::has('login'))
-                    <ul class="flex gap-8 text-xl font-mono font-bold">
-                        @auth
-                            <li class="cursor-pointer hover:text-red-200 p-2"><i class="fa-solid fa-right-to-bracket"></i> <a href="{{ url('/dashboard') }}">Back To Dashboard</a></li>
-                            @else
-                            <li class="cursor-pointer hover:text-red-200 p-2"><i class="fa-solid fa-location-dot"></i> <a href="{{ route('location') }}"> Location</a></li>
-                            <li class="cursor-pointer hover:text-red-200 p-2"><i class="fa-solid fa-house"></i> <a href="{{ route('home') }}">Home</a></li>
-                            <li class="cursor-pointer hover:text-red-200 p-2"><i class="fa-solid fa-hospital-user"></i> <a href="{{ route('services') }}">Services</a></li>
-                            <li class="cursor-pointer hover:text-red-200 p-2"><i class="fa-solid fa-hospital"></i> <a href="{{ route('about') }}">About</a></li>
-                            <li class="cursor-pointer bg-white text-red-400 rounded-md hover:text-red-700 p-2 border">
-                                @if (Route::has('register'))
-                                    <a href="{{ route('register') }}">Register</a>
-                                @endif
-                            </li>
-                        @endauth
-                    </ul>
-                @endif
-            </div>
-            
-        </header>
-        <div class="">
-           
-            <div>    
-                   ''yield dito   
-            </div>
-
-
-            
-        </div> --}}
+       
 
 
         {{-- updates --}}

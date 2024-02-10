45bcfa3deb2f2e056401de6354f389130a3b62cd
hotdog321321 <123082039+hotdog321321@users.noreply.github.com>
2024-02-10 18:47:53 +0800

    contents

--- resources/views/includes/main.blade.php
+++ resources/views/includes/main.blade.php
@@ -12,7 +12,7 @@
                     <div class="col-lg-7">
                         <div class="text pl-14">
                             <h1 class="text-4xl">RABIES</span></h1>
-                            <p class="text-black text-2xl"> <span class="text-blue-400">100% vaccine</span> preventable to save lives, to <span class="text-red-500">stop transmission</span>.</p>
+                            <p class="text-black text-2xl"> <span class="text-blue-600">100% vaccine</span> preventable to save lives, to <span class="text-red-600">stop transmission</span>.</p>
                             <div class="button">
                                 <a href="#" class="btn">Login as guest</a>
                                 <a href="#" class="btn primary login-btn">Login</a>
@@ -30,7 +30,7 @@
                     <div class="col-lg-7">
                         <div class="text pl-14">
                             <h1>RABIES VACCINATION <br> STARTS WITH YOU</span></h1>
-                            <p class="text-black text-2xl">Don't leave the issue unheard. Do your part and educate the world about it. <br> End rabies. <span class="text-blue-500">Stay healthy</span> and be safe.</p>
+                            <p class="text-black text-2xl">Don't leave the issue unheard. Do your part and educate the world about it. End rabies. <span class="text-blue-500">Stay healthy</span> and be safe.</p>
                             <div class="button">
                                 <a href="#" class="btn">Login as guest</a>
                                 <a href="#" class="btn primary login-btn">Login</a>
@@ -42,7 +42,7 @@
         </div>
         <!-- Start End Slider -->
         <!-- Start Single Slider -->
-        <div class="single-slider" style="background-image:url('https://live.staticflickr.com/2864/9890317566_3565a3e4a2_b.jpg')">
+        <div class="single-slider" style="background-image:url('https://i0.wp.com/www.purplecatvet.com/wp-content/uploads/2021/03/131565674_m.jpg?fit=2473%2C1696&ssl=1')">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-7">
@@ -107,7 +107,7 @@
                 <div class="section-title">
                     <h2>We Are Always Ready to Help You & Your Family</h2>
                     <img class="m-auto" src="img/section-img.png" alt="#">
-                    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit praesent aliquet. pretiumts</p>
+                    <p>If there's any suspicion of exposure to the rabies virus, seek medical attention immediately. Post-exposure prophylaxis (PEP) can effectively prevent the onset of symptoms if administered promptly.</p>
                 </div>
             </div>
         </div>
@@ -119,7 +119,7 @@
                         <i class="icofont icofont-ambulance-cross"></i>
                     </div>
                     <h3>Emergency Help</h3>
-                    <p class="text-white">Lorem ipsum sit, consectetur adipiscing elit. Maecenas mi quam vulputate.</p>
+                    <p>Swift Action Saves Lives. In case of potential rabies exposure, seek emergency help immediately. Time is critical for effective prevention.</p>
                 </div>
                 <!-- End Single features -->
             </div>
@@ -129,8 +129,8 @@
                     <div class="signle-icon">
                         <i class="icofont icofont-medical-sign-alt"></i>
                     </div>
-                    <h3>Enriched Pharmecy</h3>
-                    <p class="text-white">Lorem ipsum sit, consectetur adipiscing elit. Maecenas mi quam vulputate.</p>
+                    <h3>Enriched Pharmacy</h3>
+                    <p>Your Health, Our Priority: At Enriched Pharmacy, we're committed to providing expert guidance on rabies prevention. Trust us for knowledgeable advice and top-tier services.</p>
                 </div>
                 <!-- End Single features -->
             </div>
@@ -141,7 +141,7 @@
                         <i class="icofont icofont-stethoscope"></i>
                     </div>
                     <h3>Medical Treatment</h3>
-                    <p>Lorem ipsum sit, consectetur adipiscing elit. Maecenas mi quam vulputate.</p>
+                    <p>Rabies Concerns? Prompt Medical Treatment is Your Shield. Don't wait; seek professional help immediately for the right care and protection.</p>
                 </div>
                 <!-- End Single features -->
             </div>
@@ -168,7 +168,7 @@
             <div class="col-lg-3 col-md-6 col-12">
                 <!-- Start Single Fun -->
                 <div class="single-fun">
-                    <i class="icofont icofont-user-alt-3"></i>
+                    <i class="icofont-stethoscope"></i>
                     <div class="content">
                         <span class="counter">557</span>
                         <p>Specialist Doctors</p>
@@ -179,10 +179,10 @@
             <div class="col-lg-3 col-md-6 col-12">
                 <!-- Start Single Fun -->
                 <div class="single-fun">
-                    <i class="icofont-simple-smile"></i>
+                    <i class="icofont-teacher"></i>
                     <div class="content">
-                        <span class="counter">4379</span>
-                        <p>Happy Patients</p>
+                        <span class="counter">23</span>
+                        <p>Seminar</p>
                     </div>
                 </div>
                 <!-- End Single Fun -->
@@ -190,10 +190,10 @@
             <div class="col-lg-3 col-md-6 col-12">
                 <!-- Start Single Fun -->
                 <div class="single-fun">
-                    <i class="icofont icofont-table"></i>
+                    <i class="icofont icofont-user-alt-3"></i>
                     <div class="content">
                         <span class="counter">32</span>
-                        <p>Years of Experience</p>
+                        <p>Patients</p>
                     </div>
                 </div>
                 <!-- End Single Fun -->
@@ -212,7 +212,7 @@
                 <div class="section-title">
                     <h2>Different Services To Improve Your Health</h2>
                     <img src="img/section-img.png" class="m-auto" alt="#">
-                    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit praesent aliquet. pretiumts</p>
+                    <p>There are various services aimed at improving health and well-being. Here are different types of services that contribute to you.</p>
                 </div>
             </div>
         </div>
@@ -222,7 +222,7 @@
                 <div class="single-service">
                     <i class="icofont icofont-prescription"></i>
                     <h4><a href="service-details.html">Consultation</a></h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus dictum eros ut imperdiet. </p>	
+                    <p>Empowering Wellness, One Consultation at a Time. Your journey to a healthier, happier you starts with personalized health consultations. Let's prioritize your well-being together!</p>	
                 </div>
                 <!-- End Single Service -->
             </div>
@@ -231,7 +231,7 @@
                 <div class="single-service">
                     <i class="icofont-injection-syringe"></i>
                     <h4><a href="service-details.html">Anti-Rabies</a></h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus dictum eros ut imperdiet. </p>	
+                    <p>Guardian against Rabies: Protect your furry friends and loved ones. Vaccinate, Educate, Eliminate. Together, we can create a rabies-free world. </p>	
                 </div>
                 <!-- End Single Service -->
             </div>
@@ -240,7 +240,7 @@
                 <div class="single-service">
                     <i class="icofont-medicine"></i>
                     <h4><a href="service-details.html">Medicine</a></h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus dictum eros ut imperdiet. </p>	
+                    <p>Healing in a Capsule: Bridging the gap between illness and well-being. Your health, our priority.</p>	
                 </div>
                 <!-- End Single Service -->
             </div>
@@ -249,7 +249,7 @@
                 <div class="single-service">
                     <i class="icofont-teacher"></i>
                     <h4><a href="service-details.html">Free Seminar</a></h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus dictum eros ut imperdiet. </p>	
+                    <p>Nurturing Knowledge for a Healthier Tomorrow. Join our enlightening health seminar to empower yourself with insights for a vibrant and resilient life!</p>	
                 </div>
                 <!-- End Single Service -->
             </div>
@@ -267,7 +267,7 @@
                 <div class="section-title">
                     <h2>Did you know?</h2>
                     <img src="img/section-img.png" class="m-auto" alt="#">
-                    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit praesent aliquet. pretiumts</p>
+                    <p>Knowledge is the Key to Rabies Prevention. Learn, Protect, and Advocate for a world without this preventable disease.</p>
                 </div>
             </div>
         </div>
@@ -276,13 +276,16 @@
                 <!-- Single Blog -->
                 <div class="single-news">
                     <div class="news-head">
-                        <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/world-rabies-day-promo-template-design-4bf09384e69621baa0200c0946637c62_screen.jpg?ts=1600973558" alt="#">
+                        <img src="https://files01.pna.gov.ph/category-list/2023/05/03/ilocos-norte-anti-rabies-shot.jpg" alt="#">
                     </div>
                     <div class="news-body">
                         <div class="news-content">
-                            <div class="date">September 28</div>
-                            <h2><a href="blog-single.html">Rabies Awareness</a></h2>
-                            <p class="text">Lorem ipsum dolor a sit ameti, consectetur adipisicing elit, sed do eiusmod tempor incididunt sed do incididunt sed.</p>
+                            <div class="date">May 2023</div>
+                            <h2><a href="blog-single.html">Ilocos Norte records 30 rabies cases</a></h2>
+                            <p class="text">The Provincial Veterinary Office (PVO), along with the city and municipal agriculture offices, 
+                                continues to intensify its anti-rabies vaccination for dogs as rabies cases have begun to increase, with 
+                                30 cases reported so far those year.
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -292,13 +295,13 @@
                 <!-- Single Blog -->
                 <div class="single-news">
                     <div class="news-head">
-                        <img src="https://sacoronavirus.co.za/wp-content/uploads/2023/09/1080.jpg" alt="#">
+                        <img src="https://ritm.gov.ph/wp-content/uploads/2023/10/Picture1-1024x547.jpg" alt="#">
                     </div>
                     <div class="news-body">
                         <div class="news-content">
-                            <div class="date">September 25</div>
-                            <h2><a href="blog-single.html">Rabies Awareness</a></h2>
-                            <p class="text">Lorem ipsum dolor a sit ameti, consectetur adipisicing elit, sed do eiusmod tempor incididunt sed do incididunt sed.</p>
+                            <div class="date">September 2023</div>
+                            <h2><a href="blog-single.html">RabCon 2023 calls for One Health approach; highlights need for strengthened surveillance for rabies</a></h2>
+                            <p class="text">The Research Institute for Tropical Medicine - Veterinary Research Department held the second Rabies Conference in Las Piñas City on September 21-22, 2023 to discuss the collaborative efforts on surveillance and scientific advancements on rabies.</p>
                         </div>
                     </div>
                 </div>
@@ -308,13 +311,18 @@
                 <!-- Single Blog -->
                 <div class="single-news">
                     <div class="news-head">
-                        <img src="https://www.maxicare.com.ph/wp-content/uploads/2022/10/latest-announcements-rabies-vaccine-guidelines-thumbnail-c.png" alt="#">
+                        <img src="https://www.cdc.gov/rabies/animals/images/rabies-vectors-large.jpg?_=04567" alt="#">
                     </div>
                     <div class="news-body">
                         <div class="news-content">
                             <div class="date">February 2024</div>
-                            <h2><a href="blog-single.html">Rabies Treatment</a></h2>
-                            <p class="text">Lorem ipsum dolor a sit ameti, consectetur adipisicing elit, sed do eiusmod tempor incididunt sed do incididunt sed.</p>
+                            <h2><a href="blog-single.html">Animals and Rabies</a></h2>
+                            <p class="text">Rabies affects only mammals. Mammals are warm-blooded animals with fur. 
+                                People are also mammals. Birds, snakes, and fish are not mammals, so they can’t get rabies and they can’t give it to you. 
+                                But any mammal can get rabies, including people. While rabies is rare in people in the United States, with only 1 to 3 cases 
+                                reported annually, about 60,000 lives get post-exposure prophylaxis (PEP) each year to prevent rabies infection after 
+                                being bitten or scratched by an infected or suspected infected animal.
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -323,6 +331,7 @@
         </div>
     </div>
 </section>
+
 <!-- End Blog Area -->
 
 @endsection
